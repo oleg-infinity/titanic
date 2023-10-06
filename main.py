d601abf858968ac9255c2a1c8434c53b31a735d3
oleg-infinity <117585169+oleg-infinity@users.noreply.github.com>
2023-10-06 06:13:01 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ df['Age'] = df.apply(set_age, axis = 1)
 
 def set_sex(sex):
     if sex == 'male':
-        return
+        return 1
     elif sex == 'female':
         return 0
 
@@ -35,3 +35,42 @@ df[list(pd.get_dummies(df['Embarked']).columns)] = pd.get_dummies(df['Embarked']
 df.drop('Embarked', axis = 1, inplace = True)
 
 df.info()
+
+#------------------------
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+#1. Розділення набору даних на 2 частини:
+X = df.drop("Survived", axis=1)
+y = df["Survived"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+sc = StandardScaler()
+
+#2. Виконання стандартизації показників в обох наборах даних
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#3. Створення об'єкту класифікатора KNN. Налаштування параметра моделі
+classifier = KNeighborsClassifier(n_neighbors=3)
+
+#4. Навчання моделі на тренінговому наборі даних
+classifier.fit(X_train, y_train)
+
+#5. Розраховування прогнозу значень цільової змінної для тестового набору даних
+y_pred = classifier.predict(X_test)
+
+print(y_pred)
+
+for p, t in zip(y_pred, y_test):
+    print(f"p={p}; t={t}")
+
+#6. Оцінення точності прогнозу
+percent = accuracy_score(y_test, y_pred)*100
+print(percent)
+
+print(confusion_matrix(y_test, y_pred))
